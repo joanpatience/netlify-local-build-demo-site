73ed856b5d58ba07fdf47c46ac0bc47e981ee50a
joanpatience <joanpatience22@gmail.com>
2020-01-11 15:10:13 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function App(){
+    const [count, setCount] = React.useState(0)
+
+    return (
+        <div>
+            Hello from React!
+            <br/>
+            <button onClick={() => setCount(count + 1)}>Click me {count}</button>
+        </div>
+    )
+}
+
+ReactDOM.render(<App></App>, document.getElementById('app'))
